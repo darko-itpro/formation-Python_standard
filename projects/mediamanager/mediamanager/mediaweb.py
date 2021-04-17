e7a4d68fd2cf1b9228d20b49d68e7b1237939afd
Darko Stankovski <darko.itpro@gmail.com>
2021-04-17 12:25:33 +0200

    Revert "Revert "Ajout du module de configuration. Mise à jour du module web pour la prise en compte.""
    This reverts commit f483320ee52da5eb01ae13030a4586dba339985c.

--- projects/mediamanager/mediamanager/mediaweb.py
+++ projects/mediamanager/mediamanager/mediaweb.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """
 Module d'illustratin d'une interface web.
 """
 
 from flask import Flask, render_template, request, redirect
 from mediamanager import media_db
+from mediamanager import conf
 
 app = Flask(__name__)
 
@@ -20,7 +18,7 @@ def index():
 def index_show():
     if request.method == "POST":
         show_title = request.form['new_show']
-        media_db.TvShow(show_title)
+        media_db.TvShow(show_title, conf.SQLITE_PATH)
         return redirect("/shows/")
 
     else:
@@ -32,7 +30,7 @@ def index_show():
 @app.route('/show/<show_name>')
 def view_show_details(show_name):
     show_name = show_name.replace('_', ' ')
-    selected_show = media_db.TvShow(show_name)
+    selected_show = media_db.TvShow(show_name, conf.SQLITE_PATH)
     return render_template('show_detail.html', title=selected_show.name,
                            episodes=selected_show.get_episodes())
 
@@ -44,15 +42,15 @@ def add_episode(show_name):
 def episode(show_name):
     show_name = show_name.replace('_', ' ')
 
+    selected_show = media_db.TvShow(show_name, conf.SQLITE_PATH)
+
     if request.method == 'POST':
         episode_title = request.form['episode_title']
         episode_number = request.form['episode_number']
         episode_season = request.form['season_number']
-        selected_show = media_db.TvShow(show_name)
         selected_show.add_episode(episode_title, episode_number, episode_season)
         return redirect(f"/show/{show_name}")
     else:
-        selected_show = media_db.TvShow(show_name)
         return render_template('show_detail.html', title=selected_show.name,
                                episodes=selected_show.get_episodes())
 
