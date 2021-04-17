f483320ee52da5eb01ae13030a4586dba339985c
Darko Stankovski <darko.itpro@gmail.com>
2021-04-17 12:18:05 +0200

    Revert "Ajout du module de configuration. Mise à jour du module web pour la prise en compte."
    This reverts commit 8ba5dbbb3fd688b805af68865d1eef4e399c10f8.

--- projects/mediamanager/mediamanager/conf.py
+++
@@ -1,9 +0,0 @@
-"""
-Module de configuration du programme.
-
-Un module n'est chargé qu'une seule fois. Chaque modification sera
-visible par tout module ayant importé ce module.
-"""
-
-# PATHs
-SQLITE_PATH = "default.db"
--- projects/mediamanager/mediamanager/mediaweb.py
+++ projects/mediamanager/mediamanager/mediaweb.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
 Module d'illustratin d'une interface web.
 """
 
 from flask import Flask, render_template, request, redirect
 from mediamanager import media_db
-from mediamanager import conf
 
 app = Flask(__name__)
 
@@ -18,7 +20,7 @@ def index():
 def index_show():
     if request.method == "POST":
         show_title = request.form['new_show']
-        media_db.TvShow(show_title, conf.SQLITE_PATH)
+        media_db.TvShow(show_title)
         return redirect("/shows/")
 
     else:
@@ -30,7 +32,7 @@ def index_show():
 @app.route('/show/<show_name>')
 def view_show_details(show_name):
     show_name = show_name.replace('_', ' ')
-    selected_show = media_db.TvShow(show_name, conf.SQLITE_PATH)
+    selected_show = media_db.TvShow(show_name)
     return render_template('show_detail.html', title=selected_show.name,
                            episodes=selected_show.get_episodes())
 
@@ -42,15 +44,15 @@ def add_episode(show_name):
 def episode(show_name):
     show_name = show_name.replace('_', ' ')
 
-    selected_show = media_db.TvShow(show_name, conf.SQLITE_PATH)
-
     if request.method == 'POST':
         episode_title = request.form['episode_title']
         episode_number = request.form['episode_number']
         episode_season = request.form['season_number']
+        selected_show = media_db.TvShow(show_name)
         selected_show.add_episode(episode_title, episode_number, episode_season)
         return redirect(f"/show/{show_name}")
     else:
+        selected_show = media_db.TvShow(show_name)
         return render_template('show_detail.html', title=selected_show.name,
                                episodes=selected_show.get_episodes())
 
